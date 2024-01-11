d04e00f26b2550fb9bf70c4aefd775763c137ec6
Pavan kalyan <gbr8214@gmail.com>
2024-01-12 00:11:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,31 @@ from langchain.llms import OpenAI
 st.set_page_config(page_title="🦜🔗 Quickstart App")
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+# openai_api_key = st.sidebar.text_input('OpenAI API Key')
+cuisine = st.selectbox("Select Cuisine:", ["Italian", "Mexican", "Asian", "Mediterranean"])
+meat = st.selectbox("Select Meat Type:", ["Chicken", "Beef", "Pork", "Vegetarian"])
 
-def generate_response(input_text):
-  llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
-  st.info(llm(input_text))
+def generate_response(cuisine, meat):
+  service = UseLLM(service_url="https://usellm.org/api/llm")
+  # Prepare the conversation
+  prompt = f"generate one healthy recipe in {cuisine} cuisine using {meat}"
+  messages = [
+    Message(role="system", content="You are a master chef."),
+    Message(role="user", content=prompt),
+  ]
+  options = Options(messages=messages)
+  # Interact with the service
+  response = service.chat(options)
+
+  return reponse.content 
+  
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+  cuisine = st.selectbox("Select Cuisine:", ["Italian", "Mexican", "Asian", "Mediterranean"])
+  meat = st.selectbox("Select Meat Type:", ["Chicken", "Beef", "Pork", "Vegetarian"])
+  # text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
   submitted = st.form_submit_button('Submit')
-  if not openai_api_key.startswith('sk-'):
-    st.warning('Please enter your OpenAI API key!', icon='⚠')
-  if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text)
+  # if not openai_api_key.startswith('sk-'):
+  #   st.warning('Please enter your OpenAI API key!', icon='⚠')
+  if submitted:
+    generate_response(cuisine, meat)
